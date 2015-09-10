184944a0c9068466c886ee3c840a085ee07c66bd
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-09-10 14:03:17 -0400

    Updated from Brython Server: September 10, 2015 2:03:22 PM GMT-4

--- tutorial1.py
+++ tutorial1.py
@@ -14,7 +14,7 @@ noline = LineStyle(1, red)
 rectangle = RectangleAsset(500, 500, thinline, blue)
 circle = EllipseAsset(200, 200, thinline, red)
 scircle = EllipseAsset(50, 40, thinline, Color(0x073d91, 1.0))
-mouth = EllipseAsset(100, 30, thinline, Color(0x008000, 1.0))
+mouth = EllipseAsset(100, 50, thinline, Color(0x008000, 1.0))
 mouth2 = EllipseAsset(100, 30, noline, red)
 # Now display a rectangle
 Sprite(rectangle, (100,50))
