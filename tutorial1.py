ba47028582e2c40a67f263b3df1d97afe9b07ce8
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-01-31 14:51:17 -0500

    Updated from Brython Server: 1/31/2017 2:51:16 PM

--- tutorial1.py
+++ tutorial1.py
@@ -7,7 +7,7 @@ from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 # Three primary colors with no transparency (alpha = 1.0)
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
-blue = Color(0xf08080, 1.0)
+blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xf8f8ff, 1.0)
 
@@ -24,7 +24,7 @@ Sprite(frnt, (500,480))
 Sprite(cntr, (700,480))
 Sprite(back, (900,480))
 Sprite(toprim, (700,380))
-Sprite(wndw, (700, 380))
+Sprite(wndw, (705, 385))
 
 myapp = App()
 myapp.run()
