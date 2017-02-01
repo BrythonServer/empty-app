2925972332dcde37764940658d5f3e1b31f7336f
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-02-01 13:48:45 -0500

    Updated from Brython Server: 2/1/2017 1:48:45 PM

--- tutorial1.py
+++ tutorial1.py
@@ -10,6 +10,7 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xf8f8ff, 1.0)
+yellow = Color(0xffff00, 1.0)
 
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
@@ -21,7 +22,8 @@ toprim = RectangleAsset(200, 100, thinline, white)
 wndw = RectangleAsset(190, 90, thinline, blue)
 alarm = EllipseAsset(35, 25, thinline, red)
 crcle = CircleAsset(25, thinline, black)
-
+star = PolygonAsset([(20,0),(0,34.641),(-20,0)], thinline, yellow)
+star2 = PolygonAsset([(20,0),(0,-34.641),(-20,0)], thinline, yellow)
 # Now display a rectangle
 Sprite(crcle, (600,580))
 Sprite(crcle, (1000,580))
@@ -31,6 +33,8 @@ Sprite(back, (900,480))
 Sprite(toprim, (700,380))
 Sprite(wndw, (705, 385))
 Sprite(alarm, (800,355))
+Sprite(star, (800, 530))
+Sprite(star2, (800, 540))
 
 myapp = App()
 myapp.run()
