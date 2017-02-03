814a2d0c39ac0377803dc2f01acf00d7895056de
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-02-03 14:12:24 -0500

    Updated from Brython Server: 2/3/2017 2:12:24 PM

--- tutorial1.py
+++ tutorial1.py
@@ -10,7 +10,7 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xf8f8ff, 1.0)
-yellow = Color(0xffff00, 1.0)
+yellow = Color(0xffd700, 1.0)
 
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
